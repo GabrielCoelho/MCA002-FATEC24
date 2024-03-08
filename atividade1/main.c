cafa0c79423395df66b42305b30d9d5aa9bc8653
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2024-03-08 11:28:48 -0300

    Finalização da atividade

--- atividade1/main.c
+++ atividade1/main.c
@@ -3,21 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int novo_angulo() {
-  int x;
+double converte_radiano(double x) { return (x * M_PI / 180); }
+double novo_angulo() {
+  double x;
   printf("Digite o novo ângulo: ");
-  scanf("%d", &x);
+  scanf("%lf", &x);
   return x;
 }
 int main(int argc, char *argv[]) {
-  int angulo = 90;
+  double angulo = 180.0;
   int menu = 0;
   while (menu != 9) {
     if (angulo >= 0) {
-      printf("Seno do angulo %d⁰ resulta em: %.4lf⁰\n", angulo, sin(angulo));
-      printf("Cosseno do angulo %d⁰ resulta em: %.4lf⁰\n", angulo, cos(angulo));
-      printf("Tangente do angulo %d⁰ resulta em: %.4lf⁰\n", angulo,
-             tan(angulo));
+      printf("Seno do angulo %.1lf⁰ resulta em: %.4lf⁰\n", angulo,
+             sin(converte_radiano(angulo)));
+      printf("Cosseno do angulo %.1lf⁰ resulta em: %.4lf⁰\n", angulo,
+             cos(converte_radiano(angulo)));
+      if (angulo == 90 || angulo == 270) {
+        printf("Tangente do angulo %.1lf⁰ resulta em: INEXISTENTE\n", angulo);
+      } else {
+        printf("Tangente do angulo %.1lf⁰ resulta em: %.4lf⁰\n", angulo,
+               fabs(tan(converte_radiano(angulo))) < 1e-6
+                   ? 0.0
+                   : tan(converte_radiano(angulo)));
+      }
     } else {
       printf("Angulo é menor do que 0, impossível realizar o cálculo\n");
     }
